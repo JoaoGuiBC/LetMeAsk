7376fdbaced8a669e05a2945c11cfc605f147f16
JoaoGuiBC <joaoguibc@gmail.com>
2021-06-21 12:47:05 -0300

    Day 1

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from './components/Button';
+
+function App() {
+  return (
+    <div>
+      <Button text="Botão 1" />
+      <Button text="Botão 1" />
+      <Button text="Botão 1" />
+    </div>
+  ); 
+}
+
+export default App;
