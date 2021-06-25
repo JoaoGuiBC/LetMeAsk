fc7b1fcc35bec197ce5ce12497e7402823afd06a
JoaoGuiBC <joaoguibc@gmail.com>
2021-06-24 23:43:21 -0300

    Day 4 - Structure of admin room (HTML + CSS)

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,10 +2,12 @@ import { ButtonHTMLAttributes } from "react";
 
 import '../styles/components/button.scss';
 
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  isOutlined?: boolean;
+};
 
-export const Button: React.FC<ButtonProps> = (props) => {
+export const Button: React.FC<ButtonProps> = ({ isOutlined = false, ...rest }) => {
   return (
-    <button className="button" {...props} />
+    <button className={`button ${isOutlined ? 'outlined' : ''}`} {...rest} />
   );
 }
